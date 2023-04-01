ba25c2bed6efe0f68ac8867ac07585db023a2a44
Kumar Saksham <everydaycodings@gmail.com>
2023-04-01 10:45:22 +0530

    created a download_button functiion

--- helper.py
+++ helper.py
@@ -17,6 +17,20 @@ def update_secondary_col(primary_col):
     return secondary_format_list
 
 
+def download_button(encoded, file_name):
+
+    now = datetime.now()
+    formatted_date_time = now.strftime("%Y-%m-%d_%H_%M_%S")
+
+    st.markdown(
+                    f"""
+                    <a href="data:application/zip;base64,{encoded}" download="{file_name}_{formatted_date_time}.zip">
+                        <h3>Download Images</h3>
+                    </a>
+                    """,
+                    unsafe_allow_html=True,
+                )
+
 
 class ConvertPdfToX:
 
@@ -38,17 +52,7 @@ class ConvertPdfToX:
             with open(zip_path, "rb") as f:
                 encoded = base64.b64encode(f.read()).decode()
 
-            now = datetime.now()
-            formatted_date_time = now.strftime("%Y-%m-%d_%H_%M_%S")
-
-            st.markdown(
-                    f"""
-                    <a href="data:application/zip;base64,{encoded}" download="images_{formatted_date_time}.zip">
-                        <h3>Download Images</h3>
-                    </a>
-                    """,
-                    unsafe_allow_html=True,
-                )
+            download_button(encoded=encoded, file_name="images")
 
 
 class ConvertImageToPdf:
@@ -78,17 +82,7 @@ class ConvertImageToPdf:
             with open(zip_path, "rb") as f:
                 encoded = base64.b64encode(f.read()).decode()
 
-            now = datetime.now()
-            formatted_date_time = now.strftime("%Y-%m-%d_%H_%M_%S")
-
-            st.markdown(
-                    f"""
-                    <a href="data:application/zip;base64,{encoded}" download="pdf_{formatted_date_time}.zip">
-                        <h3>Download PDF</h3>
-                    </a>
-                    """,
-                    unsafe_allow_html=True,
-                )
+            download_button(encoded=encoded, file_name="pdf")
 
 
 class ConvertImageToImage:
@@ -120,15 +114,5 @@ class ConvertImageToImage:
             with open(zip_path, "rb") as f:
                 encoded = base64.b64encode(f.read()).decode()
 
-            now = datetime.now()
-            formatted_date_time = now.strftime("%Y-%m-%d_%H_%M_%S")
-
-            st.markdown(
-                    f"""
-                    <a href="data:application/zip;base64,{encoded}" download="converted_images_{formatted_date_time}.zip">
-                        <h3>Download Images</h3>
-                    </a>
-                    """,
-                    unsafe_allow_html=True,
-                )
+            download_button(encoded=encoded, file_name="converted_images")
                 
